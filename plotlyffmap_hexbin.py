36c862fff3e6169caa93dd55dd3fe2f413bf222e
Andrey Shtrauss <79809117+shtrausslearning@users.noreply.github.com>
2021-05-14 11:10:14 +0300

    Create plotlyffmap_hexbin.py

---
+++ plotlyffmap_hexbin.py
@@ -0,0 +1,30 @@
+import plotly.figure_factory as ff
+
+# Hexbin Plot
+fig = ff.create_hexbin_mapbox(data_frame=df_perth,  # df
+                              lat="LATITUDE",       # latitude
+                              lon="LONGITUDE",      # longitude
+                              nx_hexagon=200,       # hexagon discretisation
+                              opacity=0.5,
+                              min_count=1,          # minimum point in hexbin required 
+                              labels={"color": "Point Count"},
+                              range_color = [0,20], 
+                              color_continuous_scale="mint",
+                              show_original_data=True, # show point data
+                              original_data_marker=dict(size=4, opacity=0.25,color='black'), # point data options
+                              zoom = 11,
+                              height=500)
+
+# Plot Aesthetics
+fig.update_layout(
+    hovermode='closest',
+    mapbox=dict(
+        accesstoken=map_token,
+        bearing=90,
+        center=dict(lat=-32, lon=115.9),
+        pitch=0,
+        zoom=11
+    )
+)
+fig.update_layout(margin={"r":0,"t":80,"l":0,"b":0},mapbox_style="light")
+fig.show()
